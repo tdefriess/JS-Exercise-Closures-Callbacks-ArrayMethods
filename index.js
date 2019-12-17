0da45aae89cf17a7f6277e92e318252f281e8725
Tyler de Friess <tyler.defriess@gmail.com>
2019-12-17 16:31:44 -0500

    firstNamesAllCaps passed

--- index.js
+++ index.js
@@ -200,8 +200,10 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let names = [];
+  runners.forEach((items) => {names.push(`${items.first_name.toUpperCase()}`)})
+  return names;
 }
 
 /**
