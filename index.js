f762ef390c1b1b9fc2c26a2c40acefba10521853
Tyler de Friess <tyler.defriess@gmail.com>
2019-12-17 17:30:11 -0500

    stretch goal passed, all challenges passed

--- index.js
+++ index.js
@@ -159,8 +159,13 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  let newList = [];
+  for (let i = 0; i < list.length; i++){
+    if (newList.includes(list[i]) == false)
+      newList.push(list[i]);
+  }
+  return callback(newList);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
@@ -223,7 +228,7 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
   let newList = runners.filter((elm) => {
     if (tShirtSize == elm.shirt_size){
       return elm;
-    } 
+    }
   })
   return newList;
 }
